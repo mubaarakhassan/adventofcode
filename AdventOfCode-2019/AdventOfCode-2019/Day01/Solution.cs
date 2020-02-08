1f3f95f08272366e8e997143e0e3f70d77c5a7d4
hassanmubaarak609@gmail.com <kinsi,1970>
2020-02-08 16:15:28 +0100

    Added day 1 of advent code.

---
+++ AdventOfCode-2019/AdventOfCode-2019/Day01/Solution.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode_2019.Day01
+{
+    class Solution
+    {
+        private const int divider = 3;
+        private const int subtracter = 2;
+
+
+        /// <summary>
+        /// Calculates the amount of fuel needed to get to santa
+        /// </summary>
+        /// <param name="unitOfMass">The list of mass</param>
+        /// <returns>The amount of fuel needed.</returns>
+        public int RocketFuelEquationPart1(string[] unitOfMass)
+        {
+            int result = 0;
+
+            foreach (var mass in unitOfMass)
+            {
+                // Take the mass and divide it by three.
+                // Round down the number (it's an int so it automatically rounds it down).
+                var module = Int32.Parse(mass) / divider;
+
+                // Subtract by two
+                module = module - subtracter;
+
+                result += module;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Calculates the amount of fuel needed to get to santa
+        /// </summary>
+        /// <param name="mass">single mass</param>
+        /// <returns>The total amount of fuel needed.</returns>
+        private int RocketFuelEquationRecursion(int mass)
+        {
+            // Take the mass and divide it by three.
+            // Round down the number (it's an int so it automatically rounds it down).
+            var module = mass / divider;
+
+            // Subtract by two
+            module = module - subtracter;
+
+            // Base case : if the value is goes lower than 0 end the recursion otherwise go further down.
+            if (module < 0)
+                return 0;
+
+            return module += RocketFuelEquationRecursion(module); ;
+        }
+
+        /// <summary>
+        ///  Calculates the amount of fuel needed to get to santa
+        /// </summary>
+        /// <param name="unitOfMass">The list of mass</param>
+        /// <returns>The amount of fuel needed.</returns>
+        public int RocketFuelEquationPart2(string[] unitOfMass)
+        {
+            int result = 0;
+
+            foreach(var mass in unitOfMass)
+            {
+                result += RocketFuelEquationRecursion(Int32.Parse(mass));
+            }
+
+            return result;
+        }
+    }
+}
---
+++ AdventOfCode-2019/AdventOfCode-2019/Program.cs
@@ -0,0 +1,28 @@
+﻿using AdventOfCode_2019.Day01;
+using System;
+using System.IO;
+
+namespace AdventOfCode_2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This will get the current WORKING directory (i.e. \bin\Debug)
+            string workingDirectory = Environment.CurrentDirectory;
+
+            // This will get the current PROJECT directory
+            //string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+
+            Solution s = new Solution();
+
+            // This will read the input file and create arrays for each new line
+            string[] input = File.ReadAllLines($"{workingDirectory}\\Day01\\input.in");
+            
+            // Calculate the fuel
+            var fuel = s.RocketFuelEquationPart2(input);
+
+            Console.WriteLine($"The fuel needed for going to space = {fuel} !");
+        }
+    }
+}
