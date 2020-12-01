c09ffc880462dc0ab37d53ba9a08704cd899f701
mubaarakhassan <hassanmubaarak609@gmail.com>
2020-12-01 19:17:29 +0100

    Added the advent of code 2020 folder

---
+++ AdventOfCode-2019/AdventOfCode-2019/Day03/Solution.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode_2019.Day03
+{
+    
+    //Definition for singly-linked list.
+    public class ListNode {
+        public int val;
+        public ListNode next;
+        public ListNode(int val=0, ListNode next=null) {
+            this.val = val;
+           this.next = next;
+        }
+    }
+   
+    public class Solution
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            // base case
+            if (head.next == null)
+            {
+                return head;
+            }
+
+            // Loop through the array until you find the base case
+            var curr = ReverseList(head.next);  // example -> 5 -> 4 -> null ---- 3 
+            
+
+
+            ListNode prev = new ListNode(curr.val, curr.next);
+            
+            // Swap the values if the next pointer is null. If it is not keep iterating.
+            while (prev.next != null ) 
+            {
+                prev = prev.next; // 5 -4-null
+            }
+
+            if(prev.next == null)
+            {
+                curr.next = new ListNode(head.val, prev);
+
+            } 
+            else
+            {
+                curr.next = new ListNode(head.val, prev.next);
+            }
+
+            return curr;
+
+        } //5 -> 4 -> 3 -> null
+    }
+}
--- AdventOfCode-2019/AdventOfCode-2019/Program.cs
+++ AdventOfCode-2019/AdventOfCode-2019/Program.cs
@@ -1,4 +1,4 @@
-﻿using AdventOfCode_2019.Day02;
+﻿using AdventOfCode_2019.Day03;
 using System;
 using System.IO;
 using System.Linq;
@@ -24,17 +24,20 @@ namespace AdventOfCode_2019
                var fuel = solutionDay1.RocketFuelEquationPart2(input);
                Console.WriteLine($"The fuel needed for going to space = {fuel} !");
            */
-            
-            string[] file = File.ReadAllLines($"{projectDirectory}\\Day02\\input.in").FirstOrDefault().Split(','); ;
-            
-            var input = file.Select(x => Int32.Parse(x)).ToArray();
 
-            Solution solutionDay2 = new Solution();
-            var result = solutionDay2.IntcodeProgram(input.ToArray(), 12, 2);
-            Console.WriteLine($"Day 1: The current value in the array = {result} !");
+            //string[] file = File.ReadAllLines($"{projectDirectory}\\Day02\\input.in").FirstOrDefault().Split(','); ;
 
-            var result2 = solutionDay2.NounAndVerbPointer(input.ToArray());
-            Console.WriteLine($"Day 2: The current value in the array = {result2} !");
+            //var input = file.Select(x => Int32.Parse(x)).ToArray();
+
+            //Solution solutionDay2 = new Solution();
+            //var result = solutionDay2.IntcodeProgram(input.ToArray(), 12, 2);
+            //Console.WriteLine($"Day 1: The current value in the array = {result} !");
+
+            //var result2 = solutionDay2.NounAndVerbPointer(input.ToArray());
+            //Console.WriteLine($"Day 2: The current value in the array = {result2} !");
+            var input = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4 , new ListNode(5 , null)))));
+            Solution s = new Solution();
+            var val = s.ReverseList(input);
         }
     }
 }
