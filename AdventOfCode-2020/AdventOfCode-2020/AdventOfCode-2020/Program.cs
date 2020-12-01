8612aaba24104e11d86c193af11cb674ef0badc8
mubaarakhassan <hassanmubaarak609@gmail.com>
2020-12-01 21:24:00 +0100

    Added day 1

---
+++ AdventOfCode-2020/AdventOfCode-2020/AdventOfCode-2020/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+var lines = System.IO.File.ReadAllLines(@"Input\day1.txt");
+
+var input = lines.Select(int.Parse).ToList();
+
+foreach (var item in input)
+{
+    var difference = 2020 - item;
+
+    if (input.Contains(difference))
+    {
+        var answer = item * difference;
+        Console.WriteLine($"part one: {item} * {difference} = {answer}");
+
+        break;
+    }
+}
+
+foreach (var item in input)
+{
+    foreach (var subitem in input)
+    {
+        var difference = 2020 - (item + subitem);
+
+        if (input.Contains(difference))
+        {
+            var answer = (item * difference) * subitem;
+            Console.WriteLine($"part two: ({item} * {difference}) * {subitem} = {answer}");
+
+            // You can break the nested loop here if you want.
+        }
+    }
+}
+
