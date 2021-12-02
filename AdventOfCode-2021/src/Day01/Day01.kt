233f6266cce95811db6739a8d56b31119efca0ff
mubaarakhassan <hassanmubaarak609@gmail.com>
2021-12-02 23:01:43 +0100

    Added day 2. Also removed some old code from day 1

--- AdventOfCode-2021/src/Day01/Day01.kt
+++ AdventOfCode-2021/src/Day01/Day01.kt
@@ -2,44 +2,13 @@ fun main() {
     fun part1(input: List<String>): Int {
         // Objective: Count the number of times a depth measurement increases from the previous measurement
         // Step 1: Read the files line by line
-        // Step 2: Retrieve the measurement value.
-        // Step 3: Compare the retrieved value if it has increased or decreased from the previous measurement.
+        // Step 2: Retrieve the current and previous measurement value.
+        // Step 3: Compare the retrieved values if it has increased or decreased from the previous measurement.
 
-        var depths = input.map { it.toInt() };
-
-        var previous = 0;
-        var current = 0;
-        var count = 0;
-
-        depths.forEachIndexed { index, element ->
-            previous = current;
-            current = element;
-
-            if(index == 0)
-                println("$element (N/A - no previous measurement)");
-            else
-                if(current > previous){
-                    count++;
-                    println("$element (increased)");
-                }
-                else
-                    println("$element (decreased)");
-        };
-
-        return count
-    }
-
-    fun part1Improved(input: List<String>): Int {
-        // Objective: Count the number of times a depth measurement increases from the previous measurement
-        // Step 1: Read the files line by line
-        // Step 2: Retrieve the measurement value.
-        // Step 3: Compare the retrieved value if it has increased or decreased from the previous measurement.
-
-        var depths = input.map { it.toInt() };
+        val depths = input.map { it.toInt() };
         var count = 0;
 
         depths.zipWithNext(){s1, s2 -> if(s1 < s2) count++;}
-
         return count;
     }
 
@@ -51,51 +20,12 @@ fun main() {
         // Step 2: Retrieve the first, second and third measurement windows and get the sum.
         // Step 3: Compare the retrieved sum value with the previous measurement and check if it has increased or decreased.
 
-        var depths = input.map { it.toInt() };
-
-        var previousSum = 0;
-        var currentSum = 0;
+        val depths = input.map { it.toInt() };
         var count = 0;
 
-        depths.forEachIndexed { index, element ->
-            var updatedIndex = index + 2;
-            if(updatedIndex < depths.size){
-                var previousPrevious = depths[updatedIndex - 2];
-                var previous = depths[updatedIndex - 1];
-                var current = depths[updatedIndex];
+        val sums = depths.windowed(3) { it.sum() }
 
-                previousSum = currentSum;
-                currentSum = (current + previous + previousPrevious);
-
-                if(updatedIndex == 2)
-                    println("$currentSum (N/A - no previous measurement)");
-                else
-                    if(currentSum > previousSum){
-                        count++;
-                        println("$currentSum (increased)");
-                    }
-                    else
-                        println("$currentSum (decreased)");
-            }
-        };
-
-        return count;
-    }
-
-    fun part2Improved(input: List<String>): Int {
-        // Objective: Start by comparing the first and second three-measurement windows. The measurements in the first
-        // window are marked A (199, 200, 208); their sum is 199 + 200 + 208 = 607. The second window is marked B (200, 208, 210);
-        // Count the number of times the sum of measurements in this sliding window increases
-        // Step 1: Read the files line by line
-        // Step 2: Retrieve the first, second and third measurement windows and get the sum.
-        // Step 3: Compare the retrieved sum value with the previous measurement and check if it has increased or decreased.
-
-        var depths = input.map { it.toInt() };
-        var count = 0;
-
-        var sums = depths.windowed(3) { it.sum() }
         sums.zipWithNext(){s1, s2 -> if(s1 < s2) count++;}
-
         return count;
     }
 
@@ -103,7 +33,4 @@ fun main() {
 
     println(part1(input))
     println(part2(input))
-
-    println(part1Improved(input))
-    println(part2Improved(input))
 }
---
+++ AdventOfCode-2021/src/Day02/Day02.kt
@@ -0,0 +1,65 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        // Objective:  The submarine can take a series of commands like forward, down and up.
+        //  - forward X increases the horizontal position by X units.
+        //  - down X increases the depth by X units.
+        //  - up X decreases the depth by X units.
+        // After following these instructions, you would have a final horizontal position and depth.
+        // What do you get if you multiply your final horizontal position by your final depth?
+
+        // Step 1: Read the files line by line.
+        // Step 2: Check if the line is going forward, down or up.
+        // Step 3: Add or decrease it to horizontal position/depth.
+        // Step 4: Multiply the final horizontal position and depth.
+
+        var horizontal = 0;
+        var depth = 0;
+
+        input.forEach{ s ->
+            var (direction, adjustment) = s.split(" ");
+            when (direction) {
+                "forward" -> horizontal += adjustment.toInt()
+                "down" -> depth += adjustment.toInt()
+                "up" -> depth -= adjustment.toInt()
+            }
+        }
+
+        return (horizontal * depth);
+    }
+
+    fun part2(input: List<String>): Int {
+        // Objective:   The submarine can take a series of commands like forward, down, up and aim.
+        //  - down X increases the aim by X units.
+        //  - up X decreases the aim by X units.
+        //  - forward X does two things:
+        //      - It increases your horizontal position by X units.
+        //      - It increases your depth by your aim multiplied by X.
+        // After following these instructions, you would have a final horizontal position and depth.
+        // What do you get if you multiply your final horizontal position by your final depth?
+
+        // Step 1: Read the files line by line.
+        // Step 2: Check if the line is going forward, down or up.
+        // Step 3: Add or decrease it to horizontal position/depth/aim.
+        // Step 4: Multiply the final horizontal position and depth.
+
+        var horizontal = 0;
+        var depth = 0;
+        var aim = 0;
+
+        input.forEach{ s ->
+            var (direction, adjustment) = s.split(" ");
+            when (direction) {
+                "forward" -> { horizontal += adjustment.toInt();  depth += aim * adjustment.toInt();}
+                "down" -> aim += adjustment.toInt();
+                "up" -> aim -= adjustment.toInt();
+            }
+        }
+
+        return (horizontal * depth);
+    }
+
+    val input = readInput("Day02/Day02")
+
+    println(part1(input))
+    println(part2(input))
+}
