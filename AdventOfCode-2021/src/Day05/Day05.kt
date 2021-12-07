cdca80a934b1b0cb7959d42e1f23765ba19e95a3
mubaarakhassan <hassanmubaarak609@gmail.com>
2021-12-07 23:09:28 +0100

    added some more comments

--- AdventOfCode-2021/src/Day05/Day05.kt
+++ AdventOfCode-2021/src/Day05/Day05.kt
@@ -21,11 +21,11 @@ fun part1(input: List<String>): Int {
     // At how many points do at least two lines overlap?
     // For now, only consider horizontal and vertical lines: lines where either x1 = x2 or y1 = y2.
 
-    // Step 1: Create the diagram.
-    // Step 2: Iterate through each line of the input.
+    // Step 1: Get the largest number
+    // Step 2: Create a multidimensional diagram with the size as the largest  (size x size).
     // Step 3: Check if the line segments is possible. Lines where either x1 = x2 or y1 = y2 is possible.
-    // Step 4: Mark the boards with the points the lines cover.
-    // Step 5: Repeat step 2, 3 and 4 until there are no more lines left.
+    // Step 4: Iterate through the lines and mark the diagram with the points.
+    // Step 5: Repeat step 4 until there are no more lines left.
     // Step 6: Count the amount of points the two lines overlap.
 
     var points = input.map { it -> it.split(" -> ").map { it -> it.split(",").map { it.toInt() } }.flatten() }.filter { it[0] == it[2] || it[1] == it[3] }
@@ -40,6 +40,14 @@ fun part1(input: List<String>): Int {
 }
 
 fun part2(input: List<String>): Int {
+    // Objective: Same as part 1 but this we will allow horizontal lines.
+
+    // Step 1: Get the largest number
+    // Step 2: Create a multidimensional diagram with the size as the largest  (size x size).
+    // Step 3: Iterate through the lines and mark the diagram with the points.
+    // Step 4: Repeat step 3 until there are no more lines left.
+    // Step 5: Count the amount of points the two lines overlap.
+
     var points = input.map { it -> it.split(" -> ").map { it -> it.split(",").map { it.toInt() } }.flatten() }
     var size = points.maxOf { it -> it.maxOf { it } }
     var diagram = Array(size + 1) { IntArray(size + 1) }
@@ -56,27 +64,27 @@ fun drawLineFromPoints(x0: Int, y0: Int, x1: Int, y1: Int, diagram: Array<IntArr
     var y = y0 - y1
 
     while (x != 0 && y != 0){
-        diagram[y + y1][x + x1] = diagram[y + y1][x + x1]+1;
+        diagram[y + y1][x + x1] = diagram[y + y1][x + x1] + 1;
 
         // check if number is negative or positive
         if(x > 0)  x-- else x++
         if(y > 0)  y-- else y++
 
         if(x == 0 || y == 0){
-            diagram[y + y1][x + x1] = diagram[y + y1][x + x1]+1;
+            diagram[y + y1][x + x1] = diagram[y + y1][x + x1] + 1;
         }
     }
 
     while (x != 0 || y != 0)
     {
-        diagram[y + y1][x + x1] = diagram[y + y1][x + x1]+1;
+        diagram[y + y1][x + x1] = diagram[y + y1][x + x1] + 1;
 
         // check if number is negative or positive
         if(x == 0 && y > 0)  y--; if(y < 0) y++
         if(y == 0 && x > 0)  x--; if(x < 0) x++
 
         if(y == 0 && x == 0){
-            diagram[y + y1][x + x1] = diagram[y + y1][x + x1]+1;
+            diagram[y + y1][x + x1] = diagram[y + y1][x + x1] + 1;
         }
     }
 }
